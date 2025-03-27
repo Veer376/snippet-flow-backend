79c52be97263593475db3ca524416876b34e5702
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2025-03-27 22:02:19 +0530

    updating the pydantic old syntax : Optional[int] to int|Node

--- schema.py
+++ schema.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel
+from typing import Optional
 
-class User(BaseModel):
+class User(BaseModel): # /get
+    id: int|None = None # it is not required but to make the queries easy we are keeping it, its optional because it will be used in /auth
     email: str
     
-class UserInDB(User):
+class UserInDB(User): # /auth
     hashed_password: str
 
-class Interaction(BaseModel):
+class Interaction(BaseModel): # /rating
     user_id: int
     snippet_id: int
     rating: int
 
-class Snippet(BaseModel):
+class Snippet(BaseModel): # /publish & /consume
+    id: str|None = None 
     text: str
-    author: str | None
+    author: str|None = None
     
