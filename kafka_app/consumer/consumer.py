6854d6e8fdb6703e4edb06664310518335fc8040
Aryaveer <aryaveer.22gcebaiml050@galgotiacollege.edu>
2025-03-29 07:36:51 +0000

    synchronous request to /consume made async

--- kafka_app/consumer/consumer.py
+++ kafka_app/consumer/consumer.py
@@ -1,21 +1,27 @@
 import os
 from confluent_kafka import Consumer, KafkaError
-import requests
-import json 
+import aiohttp
+import asyncio
+import json
 
 BROKER = os.getenv("BOOTSTRAP_SERVERS", "kafka:9092")
 TOPIC = "new-snippet"
 
 conf = {
-        'bootstrap.servers': BROKER,
-        'group.id': 'my-consumer-group',
-        'auto.offset.reset': 'earliest'
-    }
+    'bootstrap.servers': BROKER,
+    'group.id': 'my-consumer-group',
+    'auto.offset.reset': 'earliest'
+}
 
 consumer = Consumer(conf)
 consumer.subscribe([TOPIC])
 
-try:
+async def trigger(data: dict):
+    async with aiohttp.ClientSession() as session:
+        async with session.post("http://fastapi:8000/snippet/consume", json=data) as response:
+            return response.status
+
+async def consume_messages():
     while True:
         msg = consumer.poll(timeout=1.0)
         if msg is None:
@@ -27,7 +33,23 @@ try:
             else:
                 print(f"Consumer error: {msg.error()}")
             continue
-        requests.post("http://fastapi:8000/snippet/consume", json=json.loads(msg.value().decode('utf-8')))
 
+        retry = 2
+        while retry > 0:
+            try:
+                data = json.loads(msg.value().decode('utf-8'))
+                status = await trigger(data)
+                if status == 200:
+                    break
+                else:
+                    print(f"Failed to process message, status code: {status}")
+                    retry -= 1
+            except Exception as e:
+                print(f"Error processing message: {e}")
+                retry -= 1
+
+try:
+    asyncio.run(consume_messages()) # to start the top level asyncio function
 except KeyboardInterrupt:
     print("Shutting down consumer...")
+    consumer.close()
