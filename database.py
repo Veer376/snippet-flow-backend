8ad70b3a31868e8eab61702d4b41df476f69811b
Aryaveer <aryaveer.22gcebaiml050@galgotiacollege.edu>
2025-03-31 13:48:09 +0000

    settting pool_size to 100

--- database.py
+++ database.py
@@ -6,7 +6,12 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = os.getenv('DATABASE_URL', "postgresql://username:password@postgres:5432/snippet-flow-db")
 
 # Create a SQLAlchemy engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size = 100,
+    max_overflow = 200,
+    pool_timeout = 30,
+)
 
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
